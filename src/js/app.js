66efc9c76eed02dea177af8c10b83033a0a62180
nimaiwalsh <nimai.walsh@gmail.com>
2017-05-20 10:10:26 +1000

    Finsihed API requests added ESLint

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,41 @@
-let streamApi = 'https://wind-bow.glitch.me/twitch-api/streams/';
-let twitchUsers = ['ESL_SC2', 'OgamingSC2', 'freecodecamp', 'noobs2ninjas', 'comster404'];
-let streamByUser = [];
+const streamApi = 'https://wind-bow.glitch.me/twitch-api/streams/';
+const userApi = 'https://wind-bow.glitch.me/twitch-api/users/';
+const twitchUsers = ['ESL_SC2', 'OgamingSC2', 'freecodecamp', 'noobs2ninjas', 'comster404'];
+
+//Run through GET request for each user and store returned object in Array
+let streamByUser = twitchUsers.map((user) => {
+    return fetch(`${streamApi}${user}`, {method: 'GET'})
+    .then(response => response.json())
+});
+
+let userInfo = twitchUsers.map((user) => {
+    return fetch(userApi);
+});
 
 window.onload = function() {
-
-    //Make a API request for each user and store in an array
-    twitchUsers.map((user) => {
-        fetch(streamApi + user, {method: 'GET'})
-        .then(response => response.json())
-        .then(json => {
-            streamByUser.push(json);
-            let uL = document.getElementById("user-list");        
-            let listItem = document.createElement("li");
-            listItem.className = "list-group-item";
-            if (json.stream === null) {
-                listItem.innerHTML = "null";
-            } else {
-                listItem.innerHTML = json.stream.channel.display_name;
-            }
-            uL.appendChild(listItem);   
+    //Wait until the fetch requests are complete then process the data
+    Promise
+        .all(streamByUser)
+        .then((userStream) => {
+            userStream.map(user => buildList(user));
         });
-    });
 
-    console.log(streamByUser);
+    function buildList(user) {
+        let uL = document.getElementById("user-list");        
+        let listItem = document.createElement("li");
+        listItem.className = "list-group-item";
+        if (user.stream === null) {
+            listItem.innerHTML = 'Currently no stream';
+        } else {
+            let userLogoDiv = document.createElement("div");
+            let userNameDiv = document.createElement("div");
+            userLogoDiv.className = "user-logo";
+            userLogoDiv.innerHTML = `<img src=${user.stream.channel.logo} />`;
+            listItem.appendChild(userLogoDiv);
+        }
+        uL.appendChild(listItem);
+    };
 };
+
+
+  
